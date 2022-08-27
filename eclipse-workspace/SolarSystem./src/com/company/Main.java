66e0a63e5af2d6ebdad54a5914b3764284f56a32
OlegSheremetyev <olegsheremetyev@gmail.com>
2022-08-27 13:23:30 -0400

    Feature Sun added but with mistakes

---
+++ eclipse-workspace/SolarSystem./src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main extends solarSystem {
+    public static void main(String[] args) {
+
+
+        // write your code here
+        solarSystemBatch13 inception=new solarSystemBatch13();
+        inception.Sun=1;
+        inception.Moon=4;
+        inception.Planets=2;
+
+        System.out.println("the basic structure is ready ");
+    }
+}
+
+
---
+++ eclipse-workspace/SolarSystem./src/com/company/solarSystem.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class solarSystem {
+    public static class solarSystemBatch13 {
+        int Sun;
+        int Moon;
+        int Planets;
+    }
+}
